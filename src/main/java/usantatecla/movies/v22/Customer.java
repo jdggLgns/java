6f65b535071ffbd46d6bd82bca16d882eb197ae8
jdggLgns <josedaniel.gonzalez.garcia@alumnos.upm.es>
2024-04-07 18:52:06 +0200

    Used Java Stream for getTotalCharge and getTotalFrequentRenterPoints

--- src/main/java/usantatecla/movies/v22/Customer.java
+++ src/main/java/usantatecla/movies/v22/Customer.java
@@ -36,23 +36,11 @@ public class Customer {
 	}
 
 	private double getTotalCharge() {
-		double result = 0;
-		Iterator<Rental> rentals = this.rentals.iterator();
-		while (rentals.hasNext()) {
-			Rental each = rentals.next();
-			result += each.getCharge();
-		}
-		return result;
+		return rentals.stream().mapToDouble(Rental::getCharge).sum();
 	}
-	
+
 	private int getTotalFrequentRenterPoints() {
-		int result = 0;
-		Iterator<Rental> rentals = this.rentals.iterator();
-		while (rentals.hasNext()) {
-			Rental each = rentals.next();
-			result += each.getFrequentRenterPoints();
-		}
-		return result;
+		return rentals.stream().mapToInt(Rental::getFrequentRenterPoints).sum();
 	}
 
 }
