34408f31156d4ac8160b81087846beb2a6b3b4c3
jdggLgns <josedaniel.gonzalez.garcia@alumnos.upm.es>
2024-04-07 20:42:30 +0200

    Adapted Tests (fixed error in last push)

--- src/test/java/usantatecla/movies/v22/CustomerTest.java
+++ src/test/java/usantatecla/movies/v22/CustomerTest.java
@@ -1,8 +1,8 @@
 package usantatecla.movies.v22;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CustomerTest {
 
